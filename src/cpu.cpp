79458219d416126005b5e7d8762e0a0720d6d709
Sothanforax <sothanforax@gmail.com>
2022-04-12 10:24:56 -0600

    rearranging things.

--- src/cpu.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "cpu.h"
-
-int main(){
-	struct cpu{
-		enum Reg {PC,P1,P2,P3,ACC,EXT};
-	};
-}
---
+++ src/emu.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "cpu.h"
+#include "interface.h"
+
