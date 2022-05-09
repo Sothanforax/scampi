3474bde2631b59320410e25ad2fe3ff599bc24dc
Sothanforax <sothanforax@gmail.com>
2022-05-08 19:02:50 -0600

    Added bad single stepping.

--- src/emu.cpp
+++ src/emu.cpp
@@ -14,4 +14,15 @@ int main(){
 			std::cout << "Addr: 0x" << std::hex << sizeof(memory) / sizeof(uint8_t) << std::endl;
 		}
 }
+	if(cpu.EmuState() == 2){std::cout << "SS: Enter to increment P0" << std::endl;}
+	while(cpu.EmuState() == 2){
+		std::cin.get();
+		cpu.Tick(memory[cpu.PCounterOut()]);
+		std::cout << "P0: 0x" << std::hex << cpu.PCounterOut();
+		if(cpu.PCounterOut() == sizeof(memory)/sizeof(uint8_t)){
+			cpu.SetEmuState(1);
+			std::cout << std::endl << "Out of Memory, Execution Halted." << std::endl;
+			std::cout << "Addr: 0x" << std::hex << sizeof(memory) / sizeof(uint8_t) << std::endl;
+		}
+	}
 }
